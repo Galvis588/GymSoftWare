9e622597b3496b6fa2fe5996efc8e59f31fa49bf
Andres Galvis <andresfgm588@gmail.com>
2026-09-15 10:53:12 -0500

    Se termino de resolver conflictos

--- src/models/ModelEntrenadores.js
+++ src/models/ModelEntrenadores.js
@@ -28,33 +28,3 @@ export const Entrenador = conn.define("Entrenador", {
     tableName: "entrenadores",
     timestamps: false
 });
-import { DataTypes } from "sequelize";
-import { conn } from "../config/database.js";
-
-export const Entrenador = conn.define("Entrenador", {
-
-    id_entrenador: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-
-    nombre: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-
-    edad: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-
-    especializacion: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-
-}, {
-    tableName: "entrenadores",
-    timestamps: false
-});
